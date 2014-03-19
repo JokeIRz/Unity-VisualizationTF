4fb0a7e4b34bd6c5ffbeb91690f54fbbe9e1f4b8
Ben Lancaster <iblanky@users.noreply.github.com>
2014-03-19 14:13:25 +0000

    Player is now invisble during cutscenes

--- Cutscene_Wash.cs
+++ Cutscene_Wash.cs
@@ -9,21 +9,21 @@ public class Cutscene_Wash : CutsceneController {
 			GameObject.Find ("CutsceneCamera").GetComponent<Camera>(),
 			GameObject.Find ("Tag").transform.position,
 			200, 20),
-			"2. Put hair net on here."
+			"2. Put hair net on here"
 		);
 		
 		GUI.Label(GUI2.GUI3D(
 			GameObject.Find ("CutsceneCamera").GetComponent<Camera>(),
 			GameObject.Find ("WashTag").transform.position,
 			200, 20),
-			"3. Wash hands thoroughly here."
+			"3. Wash hands thoroughly here"
 		);
 		
 		GUI.Label(GUI2.GUI3D(
 			GameObject.Find ("CutsceneCamera").GetComponent<Camera>(),
 			GameObject.Find ("ShoeTag").transform.position,
 			200, 20),
-			"1. Replace yours shoes here."
+			"1. Replace yours shoes here"
 		);
 	}
 	
@@ -31,6 +31,8 @@ public class Cutscene_Wash : CutsceneController {
 	{
 		player.GetComponent<TransparencyController>().cutscene = true;
 		icons.canGUI = false;
+		
+		player.transform.Find("Body").renderer.enabled = false;
 	}
 	
 	public override void OnEnd()
@@ -43,5 +45,7 @@ public class Cutscene_Wash : CutsceneController {
 		
 		player.GetComponent<TransparencyController>().cutscene = false;
 		icons.canGUI = true;
+		
+		player.transform.Find("Body").renderer.enabled = true;
 	}
 }
