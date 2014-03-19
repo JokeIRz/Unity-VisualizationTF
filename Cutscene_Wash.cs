11115801e14281d93ab4c65e88861767c1ed88a3
Ben Lancaster <iblanky@users.noreply.github.com>
2014-03-19 13:49:49 +0000

    Create Cutscene_Wash.cs

---
+++ Cutscene_Wash.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class Cutscene_Wash : CutsceneController {
+
+	public override void gui()
+	{
+		GUI.Label(GUI2.GUI3D(
+			GameObject.Find ("CutsceneCamera").GetComponent<Camera>(),
+			GameObject.Find ("Tag").transform.position,
+			200, 20),
+			"2. Put hair net on here"
+		);
+		
+		GUI.Label(GUI2.GUI3D(
+			GameObject.Find ("CutsceneCamera").GetComponent<Camera>(),
+			GameObject.Find ("WashTag").transform.position,
+			200, 20),
+			"3. Wash hands thoroughly here"
+		);
+		
+		GUI.Label(GUI2.GUI3D(
+			GameObject.Find ("CutsceneCamera").GetComponent<Camera>(),
+			GameObject.Find ("ShoeTag").transform.position,
+			200, 20),
+			"1. Replace yours shoes here"
+		);
+	}
+	
+	public override void OnStart()
+	{
+		player.GetComponent<TransparencyController>().cutscene = true;
+		icons.canGUI = false;
+	}
+	
+	public override void OnEnd()
+	{
+		player.SetActive(true);
+		PlayerCamera.enabled = true;
+		CutsceneCamera.enabled = false;
+		
+		player.transform.position = new Vector3(-3.89053f,0,-16.55895f);
+		
+		player.GetComponent<TransparencyController>().cutscene = false;
+		icons.canGUI = true;
+	}
+}
