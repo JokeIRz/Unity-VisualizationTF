c388e86108a90d0f246d805348d686765f0e7074
Ben Lancaster <iblanky@users.noreply.github.com>
2014-03-19 13:37:27 +0000

    Added Icons.cs

---
+++ Icons.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class Icons : MonoBehaviour
+{
+	public Hashtable icons = new Hashtable();
+	public bool canGUI = true;
+	
+	void Start()
+	{
+		foreach(Transform child in transform)
+			icons.Add(child, "<b>" + child.name + "</b>");
+	}
+	
+	void OnGUI()
+	{		
+		if(canGUI)
+		{
+			foreach(DictionaryEntry entry in icons)
+			{
+				Transform temp = entry.Key as Transform;
+				GUI.Label(GUI2.GUI3D(Camera.main, temp.position, 200), entry.Value.ToString());
+			}
+		}
+	}
+}
